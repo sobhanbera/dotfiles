d583e98b62d5faad6b02af8d659b45f1df4ad348
sobhanbera <sobhanbera258@gmail.com>
2022-12-05 06:30:51 +0530

    plugin manager changed from vim-plug to packer.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,23 @@
-require("sobhanbera.mappings")
-require("sobhanbera.utils")
+-- all the plugins config...
 require("sobhanbera.plugins")
-require("sobhanbera.setters")
-require("sobhanbera.ui")
-require("sobhanbera.prints")
+require("plugins.easymotion")
+require("plugins.fugitive")
+require("plugins.indent_blankline")
+require("plugins.lsp")
+require("plugins.lualine")
+require("plugins.null_ls")
+require("plugins.nvim_tree")
+require("plugins.telescope")
+require("plugins.treesitter")
 
 USER = vim.fn.expand("$USER")
 vim.cmd("source " .. "/Users/" .. USER .. "/.config/nvim/lua/config/markdown_preview.vim")
 vim.cmd("source " .. "/Users/" .. USER .. "/.config/nvim/lua/plugins/lastplace.vim")
 vim.cmd("source " .. "/Users/" .. USER .. "/.config/nvim/lua/plugins/maximizer.vim")
 
+require("sobhanbera.mappings")
+require("sobhanbera.utils")
+require("sobhanbera.setters")
+require("sobhanbera.ui")
+
+-- require("sobhanbera.prints")
